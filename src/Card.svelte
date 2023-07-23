2fd247cee89ea9388dd9e6a1ff28f34eab656f5d
helloitsjoe <jboyle617@gmail.com>
2023-07-22 23:43:37 -0400

    font size

--- src/Card.svelte
+++ src/Card.svelte
@@ -10,7 +10,7 @@
 
   // TODO: Use ResizeObserver, e.g. https://medium.com/@mandy.michael/resize-text-to-fit-the-parent-with-variable-font-width-axis-16921b6687ae
   $: fontSize =
-    window.innerWidth < 440 ? `${Math.max(70 / content.length, 10)}vw` : '400%';
+    window.innerWidth < 400 ? `${Math.max(70 / content.length, 10)}vw` : '400%';
 </script>
 
 <button style={`font-size: ${fontSize};`} class="content" on:click={flip}>
